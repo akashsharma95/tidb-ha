d0b5fd56c3c208df0f1b019e6dae69d17f7e0348
Akash Sharma <akashsharma@live.in>
2021-03-26 17:06:21 +0530

    fix lua script and use fetch instead of actions

--- lua/lagcheck.lua
+++ lua/lagcheck.lua
@@ -1,36 +1,43 @@
-local db_driver = require "luasql.mysql"
-local lag_threshold = 3000 -- milliseconds
-
 local function pick_backend(txn)
+    local db_driver = require "luasql.mysql"
+    local lag_threshold = 3000 -- milliseconds
+    local fe_name = txn.f:fe_name()
+
     -- default backend
     local selected_backend = 'tidb-primary'
-    local env  = db_driver.mysql()
+    local env = db_driver.mysql()
 
-    for backend_name, v in pairs(core.backends) do
-      -- ignore the default backend
-      if (backend_name ~= 'MASTER') then
-        for server_name, server in pairs(v.servers) do
-          -- if health check failed then ignore the server
-          if server:get_stats()['status'] ~= 'DOWN' then
-            -- create connection to MySQL
-            local conn = env:connect('api','root','', server:get_addr())
-            print('checking replication lag: ', server_name, server:get_addr())
-            local cur = conn:execute('SELECT ROUND(( ROUND(UNIX_TIMESTAMP(Now(6)) * 1000000) - (UNIX_TIMESTAMP(SUBSTR(ts, 1, 19)) * 1000000 + SUBSTR(ts, 21, 6))) / 1000) AS replica_lag_milli FROM heartbeat ORDER BY ts DESC LIMIT 1')
-            
-            -- check if replication lag is less that lag_threshold
-            local result = cur:fetch()
-            if result[0] < lag_threshold then
-              selected_backend = backend_name
+    for _, backend in pairs(core.backends) do
+        if backend and backend.name:sub(1, #fe_name + 1) == fe_name .. '-' then
+            for server_name, server in pairs(backend.servers) do
+                -- if health check failed then ignore the server
+                if server:get_stats()['status'] ~= 'DOWN' then
+                    -- create connection to MySQL
+                    local host, port = server:get_addr():match("([^:]+):([^:]+)")
+                    local conn = env:connect('api', 'root', '', host, port)
+                    core.Debug('checking replication lag for: ' .. server_name)
+                    core.Debug('server-addr: ' .. host .. "-" .. port)
+
+                    -- if connection failed then return default backend
+                    if conn == nil then
+                        return selected_backend
+                    end
+                    
+                    local cur = conn:execute('SELECT ROUND(( ROUND(UNIX_TIMESTAMP(Now(6)) * 1000000) - (UNIX_TIMESTAMP(SUBSTR(ts, 1, 19)) * 1000000 + SUBSTR(ts, 21, 6))) / 1000) AS replica_lag_milli FROM heartbeat ORDER BY ts DESC LIMIT 1')
+                    local lag = tonumber(cur:fetch())
+
+                    core.Debug('replication lag: ' .. lag)
+                    -- check if replication lag is less that lag_threshold
+                    if lag < lag_threshold then
+                        selected_backend = backend.name
+                    end
+                    conn:close()
+                end
             end
-            conn:close()
-          end
         end
-      end
     end
-    print('selected tidb server: ', selected_backend)
 
-    -- set selected backend to a variable
-    txn:set_var('req.tidb_backend', selected_backend)
+    return selected_backend
 end
 
-core.register_action('pick_backend', {'tcp-req', 'http-req'}, pick_backend)
+core.register_fetches('pick_backend', pick_backend)
--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 # launch mysql primary
-docker run -d --name mysql-primary -p 3306:3306 -e ALLOW_EMPTY_PASSWORD=yes bitnami/mysql:latest
+docker run --rm -d --name mysql-primary -p 4000:3306 -e ALLOW_EMPTY_PASSWORD=yes bitnami/mysql:latest
 
 # launch mysql secondary
-docker run -d --name mysql-secondary -p 3306:3306 -e ALLOW_EMPTY_PASSWORD=yes bitnami/mysql:latest
+docker run --rm -d --name mysql-secondary -p 4001:3306 -e ALLOW_EMPTY_PASSWORD=yes bitnami/mysql:latest
 
 # build haproxy image
 docker build -t haproxy -f Dockerfile .
 
 # run ha-proxy
-docker run -d --name mysql-haproxy-lb --link mysql-primary:mysql-primary --link mysql-secondary:mysql-secondary haproxy:latest
+docker run --rm -d --name mysql-haproxy-lb --link mysql-primary:mysql-primary --link mysql-secondary:mysql-secondary -p 4002:4002 -p 8404:8404 haproxy:latest
