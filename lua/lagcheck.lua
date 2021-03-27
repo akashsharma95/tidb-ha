047939fc675f19ba17dc177cbd7e232753144a78
Akash Sharma <akashsharma@live.in>
2021-03-27 15:12:45 +0530

    minor fixes

--- lua/lagcheck.lua
+++ lua/lagcheck.lua
@@ -12,9 +12,12 @@ local function pick_backend(txn)
             for server_name, server in pairs(backend.servers) do
                 -- if health check failed then ignore the server
                 local server_status = server:get_stats()['status']
-                core.Debug('backend: '.. backend.name ..' server status: ' .. server_status)
-                
+                core.Debug('backend: '.. backend.name ..', server status: ' .. server_status)
+
                 if server_status ~= 'DOWN' then
+                    -- choose first backend with server_status UP
+                    selected_backend = backend.name
+
                     -- create connection to MySQL
                     local host, port = server:get_addr():match("([^:]+):([^:]+)")
                     local conn = env:connect('api', 'root', '', host, port)
@@ -22,23 +25,26 @@ local function pick_backend(txn)
                     core.Debug('checking replication lag for: ' .. backend.name)
                     core.Debug('server-addr: ' .. host .. ":" .. port)
 
-                    -- if connection failed then return default backend
+                    -- if connection failed then break
                     if conn == nil then
-                        return selected_backend
+                        break
                     end
-                    
+
                     local cur = conn:execute('SELECT ROUND(( ROUND(UNIX_TIMESTAMP(Now(6)) * 1000000) - (UNIX_TIMESTAMP(SUBSTR(ts, 1, 19)) * 1000000 + SUBSTR(ts, 21, 6))) / 1000) AS replica_lag_milli FROM heartbeat ORDER BY ts DESC LIMIT 1')
-                    -- if fetch failed then return default backend
+                    -- if fetch failed then break
                     if cur == nil then
-                        return selected_backend
+                        break
                     end
-                    
+
                     local lag = tonumber(cur:fetch())
 
-                    core.Debug('backend: '.. backend.name ..'replication lag: ' .. lag)
+                    core.Debug('backend: '.. backend.name ..', replication lag: ' .. lag)
                     -- check if replication lag is less that lag_threshold
+                    -- choose the first backend with lag less than lag_threshold
+                    -- giving higher priority to first backend in the config
                     if lag < lag_threshold then
-                        selected_backend = backend.name
+                        core.Debug('selected backend: ' .. backend.name)
+                        return backend.name
                     end
                     conn:close()
                 end
